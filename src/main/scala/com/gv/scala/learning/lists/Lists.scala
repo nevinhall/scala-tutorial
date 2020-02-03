32f9d009db9b2aa57631d28c74de2cf2f5ff1d96
Nevin Hall <nhall@getvisibility.com>
2020-02-03 10:21:22 +0000

    Change dependencies, implement last method

--- src/main/scala/com/gv/scala/learning/lists/Lists.scala
+++ src/main/scala/com/gv/scala/learning/lists/Lists.scala
@@ -1,7 +1,18 @@
 package com.gv.scala.learning.lists
 
+import scala.annotation.tailrec
+
 object Lists {
-  def last[A](list: List[A]): A = ???
+
+  @tailrec
+  def last[A](list: List[A]): A = list match {
+    //case List.empty[A] =>
+    case Nil => throw new NoSuchElementException // empty list
+    case head :: Nil => head // single element list
+    case _ :: tail =>  last(tail) //  more than one element
+  }
+
+
 }
 
 
